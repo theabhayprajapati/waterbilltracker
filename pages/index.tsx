ff0a3cb5b32566c77ef17a44673ec6398bc8f76f
theabhayprajapati <theabhayprajapati@gmail.com>
2022-06-07 11:35:38 +0530

    added way to see the latest transactions

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,6 +12,7 @@ type monthllyBill = {
 }
 const Home: NextPage = () => {
   var today = new Date();
+
   var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
   const [totalMount, setTotalMount] = useState(0)
   const [monthlyBill, setMonthlyBill] = useState<monthllyBill>({
@@ -27,7 +28,6 @@ const Home: NextPage = () => {
     navigator.vibrate(200);
     // ping sound
     const audio = new Audio('/sound/ping.mp3')
-    audio.play()
 
 
     localStorage.setItem('monthlyBill', JSON.stringify(0))
@@ -43,6 +43,7 @@ const Home: NextPage = () => {
       }]
     })
     // set local storage
+    audio.play()
   }
   useEffect(() => {
     console.log(monthlyBill)
@@ -50,6 +51,9 @@ const Home: NextPage = () => {
     // save to session storage
     // save to local storage
     sessionStorage.setItem('monthlyBill', JSON.stringify(monthlyBill))
+    console.log(sessionStorage.getItem('monthlyBill'))
+    // save to local storage
+    
   }, [monthlyBill])
 
   // set to local storage
@@ -75,7 +79,7 @@ const Home: NextPage = () => {
   }
 
   return (
-    <div className='min-h-screen flex flex-col justify-between border-2 w-screen'>
+    <div className='bg-black min-h-screen flex flex-col justify-between w-screen'>
       {/* center heading */}
       <div className='flex justify-center items-center billStatus'>
         <div className='flex flex-col justify-center items-center'>
@@ -84,6 +88,33 @@ const Home: NextPage = () => {
           </h1>
         </div>
       </div>
+      {/* timeline */}
+      <section>
+        <h1>
+          Timeline
+        </h1>
+        <div className='p-5 showTimeline'>
+          {monthlyBill.total.sort().map((payment, index) => {
+            return (
+              <div key={index} className='flex  justify-between items-center timeLineItem'>
+                <div className='flex flex-col justify-center items-center'>
+                  <h1 className='text-4xl font-bold text-center'>
+                    {payment.amount}
+                  </h1>
+                </div>
+
+                <div className='flex flex-col justify-center items-center'>
+                  <h1 className='text-4xl font-bold text-center'>
+                    {payment.date}
+                  </h1>
+                </div>
+              </div>
+            )
+          }
+          )}
+
+        </div>
+      </section>
 
 
       {/* Add water button */}
